c941e59f66f1e4ebbe70716376452f9d5000a2f8
Mati Halperin <matias.halperin@gmail.com>
2017-11-21 12:26:36 -0300

    AOSCP bringup

--- m8-common.mk
+++ m8-common.mk
@@ -38,8 +38,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
-    camera.msm8974 \
-    Snap
+    camera.msm8974
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
